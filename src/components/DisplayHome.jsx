4eae88e9d62d3ac844f1954a400ba5840be448d7
Hamza Zubair <hamza@webevis.com>
2024-07-16 18:59:18 +0500

    update

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,6 +1,8 @@
-import { albumsData } from "../assets/assets";
+/* eslint-disable react/no-unescaped-entities */
+import { albumsData, songsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
 import Navbar from "./Navbar";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
@@ -8,17 +10,31 @@ const DisplayHome = () => {
       <Navbar />
       <div className="mb-4">
         <h1 className="my-5 font-bold text-2xl ">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((elem) => (
+            <AlbumItem
+              image={elem.image}
+              desc={elem.desc}
+              name={elem.name}
+              key={elem.id}
+              id={elem.id}
+            />
+          ))}
+        </div>
       </div>
-      <div className="flex overflow-auto">
-        {albumsData.map((elem) => (
-          <AlbumItem
-            image={elem.image}
-            desc={elem.desc}
-            name={elem.name}
-            key={elem.id}
-            id={elem.id}
-          />
-        ))}
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl ">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((elem) => (
+            <SongItem
+              image={elem.image}
+              desc={elem.desc}
+              name={elem.name}
+              key={elem.id}
+              id={elem.id}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,13 +1,9 @@
-import { useNavigate } from "react-router-dom";
-
 /* eslint-disable react/prop-types */
-const AlbumItem = ({ image, name, id, desc, ...props }) => {
-  const navigate = useNavigate();
+const SongItem = ({ image, name, id, desc, ...props }) => {
   return (
     <div
       className="min-w-[180px] p-2 px-3 rounded cursor-pointer transition ease-in-out delay-[0.3] bg-[#121212] hover:bg-[#ffffff26]"
       {...props}
-      onClick={() => navigate(`/album/${id}`)}
     >
       <img className="rounded" src={image} alt="" />
       <p className="font-bold mt-2 mb-1">{name}</p>
@@ -16,4 +12,4 @@ const AlbumItem = ({ image, name, id, desc, ...props }) => {
   );
 };
 
-export default AlbumItem;
+export default SongItem;
